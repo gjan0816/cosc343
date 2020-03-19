789b88a35dc54ccb9832a7cb5a638d20a5185064
gjan0816 <61767591+gjan0816@users.noreply.github.com>
2020-03-20 09:44:15 +1300

    Update check_edges.py

--- check_edges.py
+++ check_edges.py
@@ -4,60 +4,72 @@ from ev3dev2.motor import *
 from time import sleep, time
 from ev3dev2.sensor.lego import ColorSensor, SoundSensor
 
-cl = ColorSensor()
-cl.mode = 'COL-COLOR'
-sou = Sound()
-count_1 = 0
-count_2 = 0
-count_3 = 0
-count_4 = 0
 
-mB = LargeMotor('outB')
-mC = LargeMotor('outC')
+class Robot:
 
-while True:
-    if cl.value() == 1 and count_2 == 0:
-        count_1 += 1
-        mB.run_to_rel_pos(position_sp=10, speed_sp=100, stop_action="brake")
-    elif cl.value()!=1 and count_2 == 0:
-        # print(count_1)
-        sou.speak("count one is", count_1)
-        count_2=count_1
-        mB.stop()
-    elif count_2 != 0 :
-        for i in range (count_2, 0, -1) :
-            count_2-=1
-            mB.run_to_rel_pos(position_sp = -10, speed_sp = 100)
-        print(count_2)
-        mB.stop()
-        sou.speak("count two is", count_2)
-        break
+    def __init__(self):
+        self.cl = ColorSensor()
+        self.cl.mode = 'COL-COLOR'
+        self.sou = Sound()
+        self.mB = LargeMotor('outB')
+        self.mC = LargeMotor('outC')
 
-while True:
-    if cl.value() == 1 and count_4 == 0:
-        count_3 += 1
-        mC.run_to_rel_pos(position_sp=10, speed_sp=100, stop_action="brake")
-    elif cl.value()!= 1 and count_4 == 0:
-        # print(count_1)
-        sou.speak("count one is", count_3)
-        count_4=count_3
-        mC.stop()
-    elif count_4 != 0 :
-        for i in range (count_4, 0, -1) :
-            count_4-=1
-            mC.run_to_rel_pos(position_sp = -10, speed_sp = 100)
-        print(count_4)
-        mC.stop()
-        sou.speak("count two is", count_4)
-        break
+    def turn_right(self, count_left, count_right):
+        while True:
+            if self.cl.value() == 1 and count_left == 0:
+                count_right += 1
+                self.mB.run_to_rel_pos(position_sp=10, speed_sp=100, stop_action="brake")
+            elif self.cl.value() != 1 and count_left == 0:
+                # print(count_1)
+                self.sou.speak("count one is", count_right)
+                count_left = count_right
+                self.mB.stop()
+            elif count_left != 0:
+                for i in range(count_left, 0, -1):
+                    count_left -= 1
+                    self.mB.run_to_rel_pos(position_sp=-10, speed_sp=100)
+                print(count_left)
+                self.mB.stop()
+                self.sou.speak("count two is", count_left)
+                return count_right
+                break
 
-    # sou.speak("count two is", count_2)
+        # sou.speak("count two is", count_2)
 
-    # if cl.value() == 1 and count_2 == 0:
-    #     count_2 += 1
-    #     mC.run_to_rel_pos(position_sp =10, speed_sp = 100, stop_action = "brake")
-    # elif cl.value() != 1and count_2 != 0:
-    #     mC.run_to_rel_pos(position_sp = 10, speed_sp=-100, stop_action = "brake")
-    #
-    # if cl.value() == 1 and count_2 != 0 and count_1 != 0:
-    #     sleep(5)
+        # if cl.value() == 1 and count_2 == 0:
+        #     count_2 += 1
+        #     mC.run_to_rel_pos(position_sp =10, speed_sp = 100, stop_action = "brake")
+        # elif cl.value() != 1and count_2 != 0:
+        #     mC.run_to_rel_pos(position_sp = 10, speed_sp=-100, stop_action = "brake")
+        #
+        # if cl.value() == 1 and count_2 != 0 and count_1 != 0:
+        #     sleep(5)
+
+    def turn_left(self, count_left, count_right):
+        while True:
+            if self.cl.value() == 1 and count_right == 0:
+                count_left += 1
+                self.mC.run_to_rel_pos(position_sp=10, speed_sp=100, stop_action="brake")
+            elif self.cl.value() != 1 and count_right == 0:
+                # print(count_1)
+                self.sou.speak("count one is", count_left)
+                count_right = count_left
+                self.mC.stop()
+            elif count_right != 0:
+                for i in range(count_right, 0, -1):
+                    count_right -= 1
+                    self.mC.run_to_rel_pos(position_sp=-10, speed_sp=100)
+                print(count_right)
+                self.mC.stop()
+                self.sou.speak("count two is", count_right)
+                return count_left
+                break
+
+
+if __name__ == "__main__":
+    r = Robot()
+    check_right = r.turn_right(0, 0)
+    check_left = r.turn_left(0, 0)
+
+
+
